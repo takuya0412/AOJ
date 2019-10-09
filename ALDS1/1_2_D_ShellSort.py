1d4af31141d54ea1b2f7b2f3e61281a241838c50
Takuya <lukefone.story@gmail.com>
2019-10-09 23:40:15 +0900

    ok:shellsort

--- ALDS1/1_2_D_ShellSort.py
+++ ALDS1/1_2_D_ShellSort.py
@@ -3,12 +3,20 @@ in_data = [0]*N
 for i in range(N):
     in_data[i] = int(input().strip())
 cnt = 0
+
+def create_shell_list(N):
+    data = [1]
+    for i in range(1,101):
+        tmp = 3*data[0]+1
+        if tmp>N or len(data)==100:
+            break
+        data.insert(0,tmp)
+    return data
+
 def insertion_sort(g):
     global cnt
     global in_data
     for i in range(g,N):
-        print("i:",i)
-        print(in_data)
         v = in_data[i]
         j = i-g
         while j>=0 and in_data[j] > v:
@@ -17,14 +25,13 @@ def insertion_sort(g):
             cnt +=1
         in_data[j+g] = v
 
-def shell_sort(m,G):
-    for j in range(m-1):
+def shell_sort(G):
+    for j in G:
         insertion_sort(j)
-G=[1]
-m = len(G)
-shell_sort(m,G)
+G=create_shell_list(N)
+shell_sort(G)
 
-print(m)
+print(len(G))
 print(*G)
 print(cnt)
 for i in in_data:
