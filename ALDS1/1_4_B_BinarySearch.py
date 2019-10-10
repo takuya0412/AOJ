4d9756d786196fc64a754f9112b27ff57a395331
Takuya <lukefone.story@gmail.com>
2019-10-11 01:44:52 +0900

    20191010task

--- ALDS1/1_4_B_BinarySearch.py
+++ ALDS1/1_4_B_BinarySearch.py
@@ -3,36 +3,26 @@ S = list(map(lambda x:int(x),input().strip().split(' ')))
 q = int(input().strip())
 T = list(map(lambda x:int(x),input().strip().split(' ')))
 
-# def bin_conpare(data,n,tgt):
-#     n=-(-n//2)
-#     print("n",n)
-#     print("data\n",data)
-#     if n == 1:
-#         if data[0] == tgt:
-#             return True
-#         return False
-#     if data[n]>tgt:
-#         data=data[:n]
-#         return bin_conpare(data, n, tgt)
-#     elif data[n]<tgt:
-#         data=data[n:]
-#         return bin_conpare(data, n, tgt)
-#     else:
-#         return True
+def bin_search(N, i, S):
+    cnt = 0
+    min_i = 0
+    max_i = N-1
+    while True:
+        middle_i = (min_i + max_i)//2
+        if (max_i - min_i) == 0:
+            if S[middle_i]==i:
+                cnt = 1
+            return cnt
+
+        if S[middle_i] > i:
+            max_i = middle_i
+        elif S[middle_i] < i:
+            min_i = middle_i+1
+        else:
+            cnt = 1
+            return cnt
+
 cnt = 0
 for i in T:
-    while N>0:
-        N=-(-N//2)
-        if N==1:
-            if S[0]==i:
-                cnt+=1
-            break
-        else:
-            if S[N]>i:
-                S=S[:N]
-            elif S[N]<i:
-                S=S[N+1:]
-            else:
-                cnt+=1
-                break
+    cnt += bin_search(N, i, S)
 print(cnt)
