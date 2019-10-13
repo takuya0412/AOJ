6dcfd990682ab970e0baa91592ccb8ee041b70ff
Takuya <lukefone.story@gmail.com>
2019-10-13 23:57:10 +0900

    wip:hash

--- ALDS1/1_4_C_Dictionary.py
+++ ALDS1/1_4_C_Dictionary.py
@@ -2,19 +2,29 @@ N = int(input())
 dic = []
 res = []
 
-def find_dic(val):
-    global dic
-    for i in dic:
-        if i == val:
-            return "yes"
-    return "no"
 
-for i in range(N):
-    cmd, val = input().strip().split(' ')
-    if cmd == "insert":
-        dic.insert(0,val)
-    else:
-        res.append(find_dic(val))
-for i in res:
-    print(i)
+def h1(key,m):
+    return key%m
 
+def h2(key, m):
+    return 1+(key%(m-1))
+
+def h(key,i,m):
+    return (h1(key,m)+i*h2(key,m))
+
+def insert(T, key):
+    i = 0
+    while True:
+        j = h(key, i)
+        if T[i] ==  "":
+            T[j] = key
+            return j
+        else:
+            i += 1
+
+def search(T, key):
+    i = 0
+    while True:
+        j = h1(key, m)
+        if T[j] == key:
+            return j
